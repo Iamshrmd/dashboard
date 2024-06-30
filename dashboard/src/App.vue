2e6b0544506a274ef3ff3eff50e8e312e28f08ca
shrmyrahmd <shrmyrahmd@gmail.com>
2024-06-30 15:58:06 +0430

    add img visible and justify start to sidebar icons

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -11,65 +11,76 @@ export default {
           title:"dashboard",
           iconVisible:true,
           img:"src/assets/icons/home.png",
+          imgVisible:false,
           blackImg:"src/assets/icons/home-black.png"
         },
         {
           titleVisible:false,
           title:"Profile",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (1).png"
         },
         {
           titleVisible:false,
           title:"Leaderboard",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (2).png"
         },
         {
           titleVisible:false,
           title:"Order",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (3).png"
         },
         {
           titleVisible:false,
           title:"Product",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (10).png"
         },
         {
           titleVisible:false,
           title:"Sales Report",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (4).png"
         },
         {
           titleVisible:false,
           title:"Message",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (5).png"
         },
         {
           titleVisible:false,
           title:"Settings",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Group (9).png"
         },
         {
           titleVisible:false,
           title:"Signout",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (6).png"
         },
         {
           titleVisible:false,
           title:"Favourite",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (7).png"
         },
         {
           titleVisible:false,
           title:"History",
+          imgVisible:false,
           iconVisible:false,
           img:"src/assets/icons/Vector (8).png"
         },
@@ -88,12 +99,13 @@ export default {
     },
     showDashItem(i){
       this.iconImgs.forEach((icon,index) => {
-        // if (i == index) {
-        //   icon.titleVisible = !icon.titleVisible
-        // }else {
-        //   icon.titleVisible = false
-        // }
+        if (i == index) {
+          icon.imgVisible = !icon.imgVisible
+        }else {
+          icon.imgVisible = false
+        }
         icon.titleVisible= true
+        icon.iconVisible=false
       });
     }
   }
--- dashboard/src/components/Sidebar.vue
+++ dashboard/src/components/Sidebar.vue
@@ -1,19 +1,19 @@
 <template>
-  <section class="relative pt-5">
-    <aside class="sidebar transition-all  duration-500 bg-[#171821] w-[88px] rounded-xl pt-5 pb-5 flex flex-col gap-y-12 items-center">
-        <div class="flex gap-x-1">
+  <section class="relative py-12">
+    <aside class="sidebar transition-all duration-500 bg-[#171821] w-[88px] h-[800px] rounded-xl py-6 flex flex-col gap-y-10 items-center"  :class="{'w-[142px] rounded-tl-3xl rounded-bl-3xl rounded-tr-none rounded-br-none border-r border-[#2c2d33]':sideBarWidth}">
+        <div class="flex justify-items-start gap-x-1 w-full px-6">
             <span class="inline-block w-2.5 h-2.5 bg-[#EA1701] rounded-full"></span>
             <span class="inline-block w-2.5 h-2.5 bg-[#FEB002] rounded-full"></span>
             <span class="inline-block w-2.5 h-2.5 bg-[#029F04] rounded-full"></span>
         </div>
-        <div class="flex flex-col  gap-y-9 h-full w-full px-4 relative">
-            <div @click="showdashitem(i,icon.title)" @mouseenter="toggleIcon(i)" v-for="(icon,i) in iconImgs" :data-id="i" :key="i" class="iconDiv w-full h-9 flex items-center justify-around rounded-md">
-                <a href="#">
-                    <img class="w-3.5 h-3.5" :src="`${icon.img}`" >
-                    <img class="w-3.5 h-3.5" :src="`${icon.blackImg}`" >
+        <div class="flex flex-col gap-y-5 h-full w-full px-4 relative">
+            <div @click="showdashitem(i,icon.title)" @mouseenter="toggleIcon(i)" v-for="(icon,i) in iconImgs" :data-id="i" :key="i" class="iconDiv w-full h-9 flex items-center justify-around rounded-md" >
+                <a class="flex items-center" href="#">
+                    <img class="w-3.5 h-3.5" :src="`${icon.img}`" :class="{'hidden' : icon.imgVisible}">
+                    <img class="w-3.5 h-3.5" :src="`${icon.blackImg}`" :class="{'hidden' : !icon.imgVisible}">
                 </a>
-                <span v-if="icon.iconVisible" class="inline-block w-[3px] h-9 bg-[#A9DFD8] absolute right-0 rounded"></span>
                 <span v-show="icon.titleVisible" class="title text-xs font-bold capitalize text-[#87888C]">{{ icon.title }}</span>
+                <span v-if="icon.iconVisible" class="inline-block w-[3px] h-6 bg-[#A9DFD8] absolute right-0 rounded"></span>
             </div>
         </div>
     </aside>
@@ -27,34 +27,43 @@ export default {
     props:['iconImgs'],
     data() {
         return{
-            dashItem:'dashboard'
+            dashItem:'dashboard',
+            sideBarWidth:false,
         }
     },
     methods:{
         toggleIcon(i){
-            this.$emit('toggleIcon',i)
+            if (!this.sideBarWidth) {
+                this.$emit('toggleIcon',i)
+            }
         },
         showdashitem(i,title){
             this.$emit('showdashitem',i)
+            this.dashItem = title
+            console.log(this.sideBarWidth);
+            //
             const sideBar = document.querySelector('.sidebar')
             sideBar.classList.add('!w-[142px]')
-            this.dashItem = title
+            this.sideBarWidth =true
             const iconDivs = document.querySelectorAll('.iconDiv')
             iconDivs.forEach(div => {
+                div.classList.remove('bg-[#A9DFD8]')
+                div.classList.add('selectedDiv')
+                div.querySelector('.title').classList.remove('text-[#000]')
                 if (div.dataset.id == i) {
-                    console.log(div.querySelector('.title'));
-                    div.classList.toggle('bg-[#A9DFD8]')
+                    div.classList.add('bg-[#A9DFD8]')
+                    div.classList.remove('selectedDiv')
                     div.querySelector('.title').classList.toggle('text-[#000]')
                 }
             });
+            console.log(this.sideBarWidth);
         }
     }
 }
 </script>
-
 <style>
-    .selectedIcon{
-        background-color: #A9DFD8;
-        color:#000;
-    }
+ .selectedDiv{
+    justify-content: flex-start;
+    column-gap: 12px;
+ }
 </style>
