d377fc9a582831f3159a808889815d801e31165f
shrmyrahmd <shrmyrahmd@gmail.com>
2024-07-02 11:09:45 +0430

    add panel to profile

--- dashboard/src/components/Header.vue
+++ dashboard/src/components/Header.vue
@@ -6,20 +6,33 @@
                     </div>
                     <input class="w-full h-11 px-2 rounded-tr-lg rounded-br-lg bg-[#21222D] text-xs"  placeholder="Search Here" type="text">
                 </div>
-                <div class="flex items-center gap-x-4">
+                <div class="flex items-center gap-x-4 relative py-2">
                     <div class="relative">
                         <img class="w-3.5 h-4" src="../assets/header/Vector.png">
                         <span class="inline-block h-1.5 w-1.5 bg-[#FC003C] absolute top-0 right-0 rounded-full"></span>
                     </div>
-                    <img class="h-[30px] w-[30px]" src="../assets/header/Profile.png">
-                    <img class="w-3" src="../assets/header/Vector (1).png">
+                    <img @click="toggleProfilePanel"  class="h-[30px] w-[30px]" src="../assets/header/Profile.png">
+                    <img @click="toggleProfilePanel" class="arrow transform transition-all duration-500 hover:cursor-pointer w-3" src="../assets/header/Vector (1).png">
+                    <div class="panel transition-all duration-500 absolute top-full opacity-0 w-[100%] right-0 bg-[#171821] bg-opacity-80 rounded">
+                        <div class="flex items-center justify-between">
+                            <img class="w-4 h-4 grayscale-100" src="../assets/icons/icons8-female-user-26.png">
+                            <span class="text-xs">Edit Profile</span>
+                        </div>
+                    </div>
                 </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    methods:{
+        toggleProfilePanel(){
+            const arrow = document.querySelector('.arrow')
+            arrow.classList.toggle('rotate-180')
+            const panel = document.querySelector('.panel')
+            panel.classList.toggle('!opacity-100')
+        }
+    }
 }
 </script>
 
