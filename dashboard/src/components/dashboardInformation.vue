d1f203fc7b8b2caa15a8fee8886973f70c41b916
shrmyrahmd <shrmyrahmd@gmail.com>
2024-07-02 11:58:25 +0430

    remove side bar and dashboard information border radius

--- dashboard/src/components/dashboardInformation.vue
+++ dashboard/src/components/dashboardInformation.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="w-full h-screen no-scrollbar overflow-auto px-12 py-4 bg-[#171821] rounded-tr-3xl rounded-xl text-[#fff] rounded-br-3xl rounded-tl-none rounded-bl-none" >
+        <div class="w-full h-screen no-scrollbar overflow-auto px-12 py-4 bg-[#171821]" >
             <HeaderPage/>
             <Dashboard v-if="dashTitle == 'dashboard'"/>
         </div>
